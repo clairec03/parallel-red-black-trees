77a28e5f7be26600645082d79da1ab3aa27880a3
Claire Chen <clairechen@cmu.edu>
2024-04-28 16:25:28 -0400

    updated testing file to only run parallel impl

--- src/run-test.py
+++ src/run-test.py
@@ -3,7 +3,7 @@ import os
 
 test_cases = os.listdir(f"./inputs")
 test_modes = ["basic", "bulk", "random"]
-concurrency_modes = ["parallel", "sequential"]
+concurrency_mode = "parallel"
 
 # For PSC:
 # all_num_threads = [1, 2, 4, 8, 16, 32, 64, 128]
@@ -18,7 +18,6 @@ for test_mode in test_modes:
     for test_case in test_cases:
         if test_mode in test_case:
             for num_threads in all_num_threads:
-                concurrency_mode = concurrency_modes[bool(num_threads == 1)]
                 command = f"./red-black-{concurrency_mode} -f inputs/{test_case} -n {num_threads}"
                 process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
                 stdout, stderr = process.communicate()
