c1d915119c464c6a7efcb20a6a3b1853d5221efd
Claire Chen <clairechen@cmu.edu>
2024-05-02 19:01:16 -0400

    updated test case generation script to account for inserting values before deletion

--- src/test-gen.py
+++ src/test-gen.py
@@ -4,19 +4,39 @@ import os
 max_int, min_int = 2 ** 31 - 1, -2 ** 31
 operations = {
     "insert" : ["INSERT"],
-    "delete" : ["DELETE"],
     "both" : ["INSERT", "DELETE"]
 }
 
+def insert(f, op, num_ops, val_pool=None):
+    f.write(f"{op} {num_ops}\n")      
+    str_values = [str(random.randint(min_int, max_int)) for _ in range(num_ops)]
+    if val_pool is not None: val_pool.update(str_values)
+    f.write(" ".join(str_values) + "\n")
+
 def generate_test_cases(fpath, num_ops=10, max_len=1000, optype="both"):
     with open(fpath, "w") as f:
-        for _ in range(num_ops):
-            op = random.choice(operations[optype])
-            num_ops = random.randint(1, max_len)
-            f.write(f"{op} {num_ops}\n")
-            str_values = [str(random.randint(min_int, max_int)) for _ in range(num_ops)]
-            f.write(" ".join(str_values) + "\n")
-
+        if optype == "insert":
+            for _ in range(num_ops):
+                op = random.choice(operations[optype])
+                num_ops = random.randint(1, max_len)
+                insert(f, op, num_ops, val_pool=None)
+        elif optype == "both":
+            val_pool = set()
+            for _ in range(num_ops):
+                op = random.choice(operations["both"])
+                num_ops = random.randint(1, max_len)          
+                if op == "INSERT":
+                    insert(f, op, num_ops, val_pool)
+                elif op == "DELETE":
+                    prev_len = len(val_pool)                    
+                    if prev_len == 0:
+                        insert(f, "INSERT", num_ops, val_pool=None)
+                        continue
+                    str_values = random.sample(list(val_pool), min(num_ops, prev_len))
+                    val_pool.difference_update(str_values)
+                    f.write(f"{op} {len(str_values)}\n")      
+                    f.write(" ".join(str_values) + "\n")
+                    
 filename = input("Enter the name of the file to write the test cases to: ")
 num_ops = input("Enter the number of operations to generate: ")
 if len(num_ops) == 0:
@@ -30,7 +50,7 @@ if int(max_len) < 1:
     print("Invalid maximum length. Defaulting to 1000.")
     max_len = 1000
 
-user_op_input = input("Enter the operation type (insert, delete, both): ").strip().lower()
+user_op_input = input("Enter the operation type (insert, both): ").strip().lower()
 
 if user_op_input not in operations:
     print("Invalid operation type. Defaulting to both.")
